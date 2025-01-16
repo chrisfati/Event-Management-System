b1404557cdca4f8fe6d502799acece75c03180d4
Chrisfati <wanyamachrispinus9@gmail.com>
2025-01-16 23:33:41 +0300

    Create getEvents.php

---
+++ getEvents.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM events";
+$result = $conn->query($sql);
+
+$events = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $events[] = $row;
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($events);
+
+$conn->close();
+?>
